ea2df9ccc3f5220e4eb7984670d0f9cb6a8ec11b
CountStarlight <countstarlight@gmail.com>
2019-04-06 13:24:03 +0800

    Run format to js and html.
    Signed-off-by: CountStarlight <countstarlight@gmail.com>

--- cmd/native/main.go
+++ cmd/native/main.go
@@ -26,7 +26,7 @@ func makeBasicControlsPage() ui.Control {
 	group.SetMargined(true)
 	vbox.Append(group, true)
 
-group.SetChild(ui.NewNonWrappingMultilineEntry())
+	group.SetChild(ui.NewNonWrappingMultilineEntry())
 
 	entryForm := ui.NewForm()
 	entryForm.SetPadded(true)
